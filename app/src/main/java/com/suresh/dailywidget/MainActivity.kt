1186682dd2a8ad0a24313a463164369921db5c94
SalaSuresh <suresh.sala1992@gmail.com>
2023-05-03 17:26:17 +0530

    misc

--- app/src/main/java/com/suresh/dailywidget/MainActivity.kt
+++ app/src/main/java/com/suresh/dailywidget/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.suresh.dailywidget
 
-import android.R.style.Widget
-import android.app.PendingIntent
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonAction: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val configIntent:Intent = intent
         buttonAction = findViewById(R.id.buttonAction)
         buttonAction.setOnClickListener {
             updateWidget()
@@ -21,15 +17,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateWidget() {
-        val updateWidget =
-            Intent(this@MainActivity, Widget::class.java)
-        updateWidget.action = "updateWidget"
-        val pending = PendingIntent.getBroadcast(
-            this@MainActivity,
-            0,
-            updateWidget,
-            PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-        pending.send()
+        Toast.makeText(this, "Update Widget", Toast.LENGTH_SHORT).show()
     }
 }
