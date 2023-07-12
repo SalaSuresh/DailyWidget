4a89c083e54f24b392fa7b59994e056b93ec3af3
SalaSuresh <suresh.sala1992@gmail.com>
2023-07-12 10:21:44 +0530

    misc

--- app/src/main/java/com/suresh/dailywidget/SettingsFragment.kt
+++ app/src/main/java/com/suresh/dailywidget/SettingsFragment.kt
@@ -2,10 +2,24 @@ package com.suresh.dailywidget
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.NavHostFragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                NavHostFragment.findNavController(this@SettingsFragment).navigateUp();
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this, onBackPressedCallback
+        )
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
